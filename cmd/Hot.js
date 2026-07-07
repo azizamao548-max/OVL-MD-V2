3d9ee6729cd2a2c0859e268dc1d10ab8e0d7adba
azizamao548-max <azizamao548@gmail.com>
2026-07-07 21:41:54 +0000

    Create Hot.js

---
+++ cmd/Hot.js
@@ -0,0 +1,15 @@
+const { ovl } = require('../lib/ovl');
+
+const china = [
+"https://i.imgur.com/cn1.jpg","https://i.imgur.com/cn2.jpg","https://i.imgur.com/cn3.jpg","https://i.imgur.com/cn4.jpg","https://i.imgur.com/cn5.jpg",
+"https://i.imgur.com/cn6.jpg","https://i.imgur.com/cn7.jpg","https://i.imgur.com/cn8.jpg","https://i.imgur.com/cn9.jpg","https://i.imgur.com/cn10.jpg"
+];
+
+const japan = [
+"https://i.imgur.com/jp1.jpg","https://i.imgur.com/jp2.jpg","https://i.imgur.com/jp3.jpg","https://i.imgur.com/jp4.jpg","https://i.imgur.com/jp5.jpg",
+"https://i.imgur.com/jp6.jpg","https://i.imgur.com/jp7.jpg","https://i.imgur.com/jp8.jpg","https://i.imgur.com/jp9.jpg","https://i.imgur.com/jp10.jpg"
+];
+
+const korea = [
+"https://i.imgur.com/kr1.jpg","https://i.imgur.com/kr2.jpg","https://i.imgur.com/kr3.jpg","https://i.imgur.com/kr4.jpg","https://i.imgur.com/kr5.jpg",
+"https://i.imgur.com/kr6
